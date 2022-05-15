ece9df7ffd99f6b43ac2f844c0d53ac24717637a
zhmanastasiya <zhmanastasiya@mail.ru>
2022-05-15 17:19:55 +0400

    second task

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,11 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	input2 := make([]int64, len(input))
+	copy(input2, input)
+	for i := len(input2)/2 - 1; i >= 0; i-- {
+		opp := len(input2) - 1 - i
+		input2[i], input2[opp] = input2[opp], input2[i]
+	}
+	return input2
 }
